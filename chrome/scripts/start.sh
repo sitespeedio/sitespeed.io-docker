3b466cfa7acc2000f2a8aabf92df24070678f979
soulgalore <peter@soulgalore.com>
2015-04-06 12:57:08 +0200

    hello new way to start the selenium server

--- chrome/scripts/start.sh
+++ chrome/scripts/start.sh
@@ -8,6 +8,6 @@ google-chrome-stable --version
 echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
-echo 'Starting Selenium standalone server ...'
-2>/dev/null 1>&2 java -jar /home/root/selenium-server-standalone-2.45.0.jar &
+#su - seleniumuser
+su - seleniumuser -c '2>/dev/null 1>&2 java -jar /home/root/selenium-server-standalone-2.45.0.jar &'
 exec "$@"
