334ca920458a9681a52833a4e0299aeb7cdc8194
soulgalore <peter@soulgalore.com>
2015-04-06 13:47:10 +0200

    new go

--- chrome/scripts/start.sh
+++ chrome/scripts/start.sh
@@ -9,5 +9,5 @@ echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
 echo 'Starting Selenium server ...'
-2>/dev/null 1>&2 java -jar /home/root/selenium-server-standalone-2.45.0.jar &
+2>/dev/null 1>&2 java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar /home/root/selenium-server-standalone-2.45.0.jar &
 exec "$@"
