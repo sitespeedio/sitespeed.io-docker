ad6a364aac6f6f704e63582c074a8842e559c2cd
soulgalore <peter@soulgalore.com>
2015-04-16 00:18:29 +0200

    reverted

--- all/scripts/start.sh
+++ all/scripts/start.sh
@@ -12,6 +12,6 @@ firefox --version 2>/dev/null
 echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
-# echo 'Starting Selenium server ... access it at http://127.0.0.1:4444/wd/hub'
-# 2>/dev/null 1>&2 nohup java -jar /home/root/selenium-server-standalone-2.45.0.jar -Djava.security.egd=file:/dev/./urandom > /tmp/selenium.log &
+echo 'Starting Selenium server ... access it at http://127.0.0.1:4444/wd/hub'
+2>/dev/null 1>&2 nohup java -jar /home/root/selenium-server-standalone-2.45.0.jar -Djava.security.egd=file:/dev/./urandom > /tmp/selenium.log &
 exec "$@"
