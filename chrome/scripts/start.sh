7bfc217ca677dfa43d13c5e484b1e5955df0df8a
soulgalore <peter@soulgalore.com>
2015-06-07 13:30:09 +0200

    hey use the new selemium version

--- chrome/scripts/start.sh
+++ chrome/scripts/start.sh
@@ -9,5 +9,5 @@ echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
 echo 'Starting Selenium server ... access it at http://127.0.0.1:4444/wd/hub'
-2>/dev/null 1>&2 nohup java -jar /home/root/selenium-server-standalone-2.45.0.jar -Djava.security.egd=file:/dev/./urandom > /tmp/selenium.log &
+2>/dev/null 1>&2 nohup java -jar /home/root/selenium-server-standalone-2.46.0.jar -Djava.security.egd=file:/dev/./urandom > /tmp/selenium.log &
 exec "$@"
