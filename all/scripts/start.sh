ce0bc7b240212e0f49ee216e948f7267225c8b04
soulgalore <peter@soulgalore.com>
2016-05-19 18:15:20 +0200

    use sitespeed.io 4.0 branch

--- all/scripts/start.sh
+++ all/scripts/start.sh
@@ -11,6 +11,4 @@ firefox --version 2>/dev/null
 echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99 -shmem -screen 0 1366x768x16 &
-echo 'Starting Selenium server ... access it at http://127.0.0.1:4444/wd/hub'
-2>/dev/null 1>&2 nohup java -jar /home/root/selenium-server-standalone-2.48.2.jar -Djava.security.egd=file:/dev/./urandom > /tmp/selenium.log &
 exec "$@"
